d677e430ccb581c7d439ddb3449b08c0c954af8b
KOBAYASHI atsushi <kobaatsu@gmail.com>
2018-11-29 19:03:45 +0900

    svg store タスク使えるように

--- tasks/SvgStore.js
+++ tasks/SvgStore.js
@@ -7,37 +7,35 @@ const svgo = require('gulp-svgo');
 const svgstore = require('gulp-svgstore');
 const rename = require('gulp-rename');
 
-const svgStore = (src, dist) => {
-  src.forEach(elm => {
-    return gulp
-      .src(elm.src)
-      .pipe(
-        plumber({
-          errorHandler:
-            notify.onError({
-              title: 'SVG Error: Line <%= error.line %>',
-              message: '<%= error.message %>'
-            })
-        })
-      )
-      .pipe(
-        svgo({
-          plugins: [
-            { removeTitle: true },
-            { removeAttrs: { attrs: '(fill|stroke|data.*)' } },
-            { removeStyleElement: true },
-            { cleanupIDs: false },
-          ]
-        })
-      )
-      .pipe(
-        svgstore({ inlineSvg: true })
-      )
-      .pipe(
-        rename({ basename: elm.dist })
-      )
-      .pipe(gulp.dest(dist));
-  });
+const svgStore = (src, dist, name) => {
+  return gulp
+    .src(src)
+    .pipe(
+      plumber({
+        errorHandler:
+          notify.onError({
+            title: 'SVG Error: Line <%= error.line %>',
+            message: '<%= error.message %>'
+          })
+      })
+    )
+    .pipe(
+      svgo({
+        plugins: [
+          { removeTitle: true },
+          { removeAttrs: { attrs: '(fill|stroke|data.*)' } },
+          { removeStyleElement: true },
+          { cleanupIDs: false },
+        ]
+      })
+    )
+    .pipe(
+      svgstore({ inlineSvg: true })
+    )
+    .pipe(
+      rename({ basename: name })
+    )
+    .pipe(gulp.dest(dist));
 }
 
 module.exports = svgStore;
